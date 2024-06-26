d75d728ef05ce1d95402ad66e211a8ca3f958d34
rosar <rosario.amantia@outlook.it>
2024-06-26 19:48:18 +0200

    login progress

--- app/src/main/java/com/rosario/notetakingcompose/login/Login.kt
+++ app/src/main/java/com/rosario/notetakingcompose/login/Login.kt
@@ -1,2 +1,13 @@
 package com.rosario.notetakingcompose.login
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+
+@Composable
+fun LoginScreen(
+    loginViewModel: LoginViewModel? = null,
+    onNavToHomePage:() -> Unit,
+    onNavToSignupPage:() -> Unit,
+){
+        val loginState = loginViewModel?.loginState
+}
