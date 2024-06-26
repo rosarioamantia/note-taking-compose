121b0bf1a56b7ce10b6e49f640b7f185027499a1
rosar <rosario.amantia@outlook.it>
2024-06-26 21:05:13 +0200

    ui fix

--- app/src/main/java/com/rosario/notetakingcompose/login/Login.kt
+++ app/src/main/java/com/rosario/notetakingcompose/login/Login.kt
@@ -1,7 +1,17 @@
 package com.rosario.notetakingcompose.login
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.rosario.notetakingcompose.ui.theme.NoteTakingComposeTheme
 
 @Composable
 fun LoginScreen(
@@ -9,5 +19,36 @@ fun LoginScreen(
     onNavToHomePage:() -> Unit,
     onNavToSignupPage:() -> Unit,
 ){
-        val loginState = loginViewModel?.loginState
+    val loginState = loginViewModel?.loginState
+    val isError = loginState?.loginError != null
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        Text(
+           text = "Login",
+            style = MaterialTheme.typography.headlineLarge,
+            fontWeight = FontWeight.Black,
+            color = MaterialTheme.colorScheme.error
+        )
+
+        if(isError){
+            Text(
+                text = loginState?.loginError ?: "Unknown Error", //Elvis operator
+                color = Color.Red
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun PrevLoginScreen() {
+    NoteTakingComposeTheme {
+        LoginScreen(onNavToHomePage = { /*TODO*/ }) {
+
+        }
+    }
 }
